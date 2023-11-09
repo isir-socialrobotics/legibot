6bb8da2d43ba56670cecadd8e7573d200d887f51
Javad <amiryan.j@gmail.com>
2023-11-09 12:48:23 +0100

    fix projection_api.py

--- src/legibot/projection_api.py
+++ src/legibot/projection_api.py
@@ -6,28 +6,27 @@ import cv2
 def draw_projected_grid(im, grid, homog):
     grid_lines = []
     for i in range(grid[0].shape[0]):
-        grid_lines.append(np.array([[grid[0][i], grid[1][0], 1],
-                                    [grid[0][i], grid[1][-1], 1]]))
-    for i in range(grid[1].shape[0]):
-        grid_lines.append(np.array([[grid[0][0], grid[1][i], 1],
-                                    [grid[0][-1], grid[1][i], 1]]))
+        for j in range(grid[1].shape[0]):
+            if i != 0:
+                grid_lines.append([[grid[0][i - 1], grid[1][j], 1], [grid[0][i], grid[1][j], 1]])
+            if j != 0:
+                grid_lines.append([[grid[0][i], grid[1][j - 1], 1], [grid[0][i], grid[1][j], 1]])
 
     grid_lines = np.array(grid_lines, dtype=np.float32)
-    xx = (homog @ grid_lines.reshape(-1, 3).T).T.reshape(-1, 2, 3)
-    im_points = xx[:, :, :2] / xx[:, :, 2:]
+    grid_lines_proj = (homog @ grid_lines.reshape(-1, 3).T).T.reshape(-1, 2, 3)
 
-    for i in range(im_points.shape[0]):
-        for j in range(im_points.shape[1]):
-            p = im_points[i, j].astype(np.int32)
-            # print(p , grid[0][j], grid[1][i])
-            cv2.circle(im, (int(im_points[i, j, 0]), int(im_points[i, j, 1])), 3, (0, 255, 0), -1)
+    for i in range(grid_lines_proj.shape[0]):
+        l = grid_lines_proj[i]
+        p0 = (int(l[0, 0] / l[0, 2]), int(l[0, 1] / l[0, 2]))
+        p1 = (int(l[1, 0] / l[1, 2]), int(l[1, 1] / l[1, 2]))
+        cv2.line(im, p0, p1, (0, 255, 0), 1)
 
     cv2.imshow('im', im)
     cv2.waitKey(0)
 
 
 def make_homog():
-    focal_length = 600  # Focal length in pixels
+    focal_length = 600  # fixme: Focal length in pixels
     image_width = 1280
     image_height = 720
 
@@ -50,7 +49,7 @@ def make_homog():
 
     # Translation vector (height of the camera above the ground)
     camera_height = 1.6  # meters
-    T = np.array([0, 0, -camera_height])
+    T = np.array([0, 0, camera_height])
     t_world2cam = -R.T @ T
     E = [r_world[:, 0], r_world[:, 1], t_world2cam]
     E = np.vstack(E)
@@ -59,10 +58,16 @@ def make_homog():
     return h_world2cam
 
 
-def capture_click(event, x, y, flags, param):
+def make_grid():
+    xs = np.arange(0.5, 15.01, 1)
+    ys = np.arange(-3, 3.01, 1)
+    return ys, xs
+
+
+def capture_click(event, x, y, flags, param):  # print back-projected point
     if event == cv2.EVENT_LBUTTONDOWN:
-        # todo: to print back-projected point
-        pnt = np.array([1280-x, 720-y, 1])
+        homog_inv = param[0]
+        pnt = np.array([x, y, 1])
         pnt_proj = homog_inv @ pnt
         pnt_proj = [pnt_proj / pnt_proj[2]]
         pnt_proj = [pnt_proj[0][1], pnt_proj[0][0]]
@@ -70,19 +75,12 @@ def capture_click(event, x, y, flags, param):
 
 
 if __name__ == '__main__':
-    # test code
-    homog = make_homog()
-    homog_inv = np.linalg.inv(homog)
-
-    cv2.namedWindow("im")
-    cv2.setMouseCallback('im', capture_click)
-
-    # test_pnts = [[5,  0.2], [5, -0.2]]
-    # for pnt in test_pnts:
-    #     pnt_proj = homog @ np.array([pnt[0], pnt[1], 1])
-    #     pnt_proj = pnt_proj / pnt_proj[2]
+    h = make_homog()
+    h_inv = np.linalg.inv(h)
 
     im = np.zeros((720, 1280, 3), dtype=np.uint8)
-    grid = (np.arange(0, 20, 0.1), np.arange(-5, 5, 0.2))
+    cv2.namedWindow("im")
+    cv2.setMouseCallback('im', capture_click, (h_inv,))
 
-    draw_projected_grid(im, grid, homog)
+    g = make_grid()
+    draw_projected_grid(im, g, h)
