3ab879c779ccd4b39c9256f426e129e500d4d94f
Javad <amiryan.j@gmail.com>
2023-11-13 12:47:55 +0100

    Refactor codes

---
+++ src/legibot/traj_controller.py
@@ -0,0 +1,107 @@
+import math
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+from vive_ai.utils.point2d import Point2
+from vive_ai.consts.consts import *
+
+
+def angle_to_unitary(angle_rad):
+    if angle_rad > math.pi:
+        angle_rad -= 2 * math.pi
+    elif angle_rad < -math.pi:
+        angle_rad += 2 * math.pi
+    return angle_rad
+
+
+def sign(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    return 0
+
+
+class OdomHistory:
+    def __init__(self, max_size=500):
+        self.max_size = max_size
+        self.history = []
+
+    def append(self, msg: Odometry):
+        if len(self.history) > 0:
+            last_msg = self.history[-1]
+            if msg.header.stamp - last_msg.header.stamp < rospy.Duration(0.5):
+                return
+        # point = Point2(msg.pose.pose.position.x, msg.pose.pose.position.y)
+        self.history.append(msg)
+        if len(self.history) > self.max_size:
+            self.history.pop(0)
+
+    def get_trajectory(self):
+        return [Point2(msg.pose.pose.position.x, msg.pose.pose.position.y) for msg in self.history]
+
+
+class TrajectoryController:
+    def __init__(self, trajectory):
+        self.trajectory = trajectory
+        self.current_index = 0
+        self.robot_xy = None
+        self.robot_orien = -10
+        self.odom_history = OdomHistory()
+
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.rate = rospy.Rate(20)
+
+    def odom_callback(self, msg: Odometry):
+        self.robot_xy = msg.pose.pose.position
+        self.robot_orien = euler_from_quaternion([msg.pose.pose.orientation.x,
+                                                  msg.pose.pose.orientation.y,
+                                                  msg.pose.pose.orientation.z,
+                                                  msg.pose.pose.orientation.w])[2]
+        self.odom_history.append(msg)
+
+    def get_current_waypoint(self):
+        return self.trajectory[self.current_index]
+
+    @staticmethod
+    def is_reached(waypoint, pose: Point):
+        dx = waypoint.x - pose.x
+        dy = waypoint.y - pose.y
+        dist = (dx**2 + dy**2)**0.5
+        goal_threshold = 0.25
+        return dist < goal_threshold
+
+    def __calc_command__(self) -> Twist:
+        command = Twist()
+        if self.robot_xy is None:
+            return command
+        if self.current_index >= len(self.trajectory):
+            print("Trajectory finished")
+            return command
+        if self.is_reached(self.get_current_waypoint(), self.robot_xy):
+            self.current_index += 1
+            return command
+
+        next_subgoal = self.get_current_waypoint()
+        subgoal_vec = Point(next_subgoal.x - self.robot_xy.x, next_subgoal.y - self.robot_xy.y, 0)
+        angle_to_subgoal = math.atan2(subgoal_vec.y, subgoal_vec.x)
+        d_angle = angle_to_unitary(angle_to_subgoal - self.robot_orien)
+
+        # check for rotation
+        if abs(math.degrees(angle_to_unitary(angle_to_subgoal - self.robot_orien))) > 10:
+            command.linear.x = 0
+            command.angular.z = 0.2 * sign(angle_to_unitary(angle_to_subgoal - self.robot_orien))
+
+        else:
+            command.linear.x = 0.3
+            command.angular.z = angle_to_unitary(angle_to_subgoal - self.robot_orien) * 0.1  # small orien correction
+
+        return command
+
+    def exec(self):
+        command = self.__calc_command__()
+        self.cmd_vel_pub.publish(command)
+        self.rate.sleep()
