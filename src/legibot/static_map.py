4deb3616c1c8091e0368c000530a67edac3d474f
Javad <amiryan.j@gmail.com>
2024-02-01 15:47:19 +0100

    Parsing gazebo world file in static_map.py

--- src/legibot/static_map.py
+++ src/legibot/static_map.py
@@ -1,54 +1,37 @@
-# <include>
-#       <uri>model://round_table</uri>
-#       <name>table_1</name>
-#       <pose>-2.5 -8 0 0 0 1.57</pose>
-#       <static>true</static>
-#     </include>
-#
-#     <include>
-#       <uri>model://round_table</uri>
-#       <name>table_2</name>
-#       <pose>-2.5 -4 0 0 0 1.57</pose>
-#       <static>true</static>
-#     </include>
-#
-#     <include>
-#       <uri>model://person_standing</uri>
-#       <name>person_2</name>
-#       <pose>3.4 -8 0.16 0 0 -1.5707</pose>
-#       <static>true</static>
-#     </include>
-#
-#     <include>
-#       <uri>model://round_table</uri>
-#       <name>table_3</name>
-#       <pose>2 -8 0 0 0 1.57</pose>
-#       <static>true</static>
-#     </include>
-#
-#     <include>
-#       <uri>model://person_standing</uri>
-#       <name>person_3</name>
-#       <pose>-3.85 -4 0.16 0 0 1.5707</pose>
-#       <static>true</static>
-#     </include>
-#
-#     <include>
-#       <uri>model://round_table</uri>
-#       <name>round_table_1</name>
-#       <pose>2 -4.0 0 0 0 1.57</pose>
-#       <static>true</static>
-#     </include>
+import os
 import numpy as np
+import xml.etree.ElementTree as ET
 from geometry_msgs.msg import Pose, Point, Quaternion
-from tf.transformations import quaternion_from_euler
-import tf
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
 from legibot.utils.viz_utils import Visualizer
 
+def parse_gazebo_world(world_filename):
+    tree = ET.parse(world_filename)
+    root = tree.getroot().find('world')
+
+    tables_6d_pose = []
+    observers_6d_pose = []
+
+    # Find and list all elements of type "include"
+    for include_element in root.findall('.//include'):
+        pose = include_element.find('pose').text
+        pose = [float(p) for p in pose.split(' ')]
+        xyz, rpy = pose[:3], pose[3:]
+
+        if not hasattr(include_element, "uri"):
+            include_element.attrib["uri"] = include_element.find("uri").text
+
+        if include_element.attrib["uri"] == "model://round_table":
+            tables_6d_pose.append([*xyz, *rpy])
+        elif include_element.attrib["uri"] == "model://person_standing":
+            observers_6d_pose.append([*xyz, *rpy])
+
+    print(f'Tables Center XY: {tables_6d_pose}')
+    print(f'Observers XY: {observers_6d_pose}')
+    return tables_6d_pose, observers_6d_pose
 
 def parse_walls(walls_filename):
-    import xml.etree.ElementTree as ET
     walls = []
 
     tree = ET.parse(walls_filename)
@@ -82,7 +65,7 @@ def conv_walls_to_obstacles(walls):
     obs_ = []
     for wall in walls:
         for ii in np.arange(-wall[1][0] / 2, wall[1][0] / 2 + 0.1, 0.2):
-            yaw = tf.transformations.euler_from_quaternion([wall[0].orientation.x,
+            yaw = euler_from_quaternion([wall[0].orientation.x,
                                                             wall[0].orientation.y,
                                                             wall[0].orientation.z,
                                                             wall[0].orientation.w])[2]
@@ -92,23 +75,19 @@ def conv_walls_to_obstacles(walls):
                         0.1])
     return obs_
 
-observers = [
-    [2.2, -8],
-    [2.2, -4],
-    ## [-3.85, -4],
-]
 
-tables = [
-    [-2.5, -8, 0.75],
-    [-2.5, -4, 0.75],
-    [1, -8, 0.75],
-    [1, -4, 0.75],
-]
-
-walls_list = parse_walls("/home/javad/workspace/catkin_ws/src/legibot/worlds/walls.sdf")
+legibot_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+tables, persons = parse_gazebo_world(os.path.join(legibot_dir, "worlds/restaurant.world"))
+observers = [[p[0], p[1]] for p in persons]
+walls_list = parse_walls(os.path.join(legibot_dir, "worlds/walls.sdf"))
 walls_obs = conv_walls_to_obstacles(walls_list)
-obstacles = tables + walls_obs + [[p[0], p[1], 0.3] for p in observers]
+obstacles = (walls_obs
+             + [[p[0], p[1], 0.3] for p in observers]
+             + [[p[0], p[1], 0.75] for p in tables]
+             )
 
+
+tables_center_xy, observers_xy = parse_gazebo_world("/home/javad/workspace/catkin_ws/src/legibot/worlds/restaurant.world")
 
 if __name__ == '__main__':
     vis = Visualizer()
