3d9399b10aecb640850e713f89408bb8809e608d
Javad <amiryan.j@gmail.com>
2024-02-28 12:41:51 +0100

    Add clutter cylinders in legibot_main.py

--- scripts/legibot_main.py
+++ scripts/legibot_main.py
@@ -18,12 +18,12 @@ def main():
                  # (-3, -5, 30),
                  ]
 
-    empty_tables = [
-        (0, -3, 0.2),
+    clutter_tables = [
+        (0.4, -0.5, 0.2),
         # (-2.5, -5.5, 0),
     ]
     static_map = StaticMap()
-    static_map.tables = static_map.tables + empty_tables
+    static_map.tables = static_map.tables + clutter_tables
     StaticMap().update()
 
     # Scenario 2
@@ -60,15 +60,16 @@ def main():
     legibot_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     round_table_sdf = os.path.join(legibot_dir, "models/round_table/model.sdf")
     person_standing_sdf = os.path.join(legibot_dir, "models/person_standing/model.sdf")
+    clutter_cylinder_sdf = os.path.join(legibot_dir, "models/clutter_cylinder/model.sdf")
 
     # gazebo_delete_model(robot_name="table")
     gazebo_delete_model(robot_name="pepper")
     subprocess.Popen(["roslaunch", "legibot", "spawn_pepper.launch", f"x:={robot_x0[0]}", f"y:={robot_x0[1]}", f"yaw:={robot_x0[2]}"])
 
-
     gazebo_delete_model(robot_name="observer")
     gazebo_delete_model(robot_name="table")
     gazebo_delete_model(robot_name="person")
+    gazebo_delete_model(robot_name="clutter_cyl")
 
     for i in range(len(observers)):
         # subprocess.Popen(["roslaunch", "legibot", "spawn_table.launch", f"x:={tables_xy[i][0]}", f"y:={tables_xy[i][1]}", f"yaw:={observers[i][2]}"])
@@ -82,6 +83,10 @@ def main():
             gazebo_spawn_static_model(f"person__{i}", person_standing_sdf,
                                       f"{observers[i][0]}, {observers[i][1]}, 0.16, 0, 0, {math.radians(observers[i][2])+ math.pi/2}", "world")
         time.sleep(0.2)
+
+    for ii, table in enumerate(clutter_tables):
+        gazebo_spawn_static_model(f"clutter_cyl__{ii}", clutter_cylinder_sdf,
+                                  f"{table[0]}, {table[1]}, 0, 0, 0, 0", "world")
 
     # restart observer node
     record = True
